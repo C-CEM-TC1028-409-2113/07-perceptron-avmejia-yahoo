11f8dbb00beb783947bc32f4d7441c1b25c09d67
avmejia-yahoo <64789801+avmejia-yahoo@users.noreply.github.com>
2021-09-17 20:13:46 -0500

    Add files via upload

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,2 +1,13 @@
 import math
 # utiliza math.exp
+
+def salida_neurona(l1,l2):
+    if len(l1)!=len(l2):
+        return "Error"
+    else:
+        long = len(l1)
+        x=0.0;
+        for i in range(long):
+            x = x + l1[i]*l2[i] 
+        sig = 1 / (1 + math.exp(-x))               
+        return sig
